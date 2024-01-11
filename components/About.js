db666bf87014f78c38e95cbe3fd7a87c3b6e723e
Karim Hossenbux <karimhossenbux@users.noreply.github.com>
2024-01-11 21:23:47 +0100

    update resume

--- components/About.js
+++ components/About.js
@@ -1,12 +1,14 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 const About = (props) => {
-  const info = props.info
-  const twitter = info.profiles.find(profile => profile.network === 'Twitter')
+  const info = props.info;
+  const twitter = info.profiles.find(
+    (profile) => profile.network === 'Twitter'
+  );
 
   return (
     <div className="section mt-2">
-      <div className="flex items-center justify-center">
+      <div className="flex items-baseline justify-center">
         <div className="m-2 text-right">
           <h1 className="text-2xl md:text-3xl font-extrabold tracking-tight">
             {info.name}
@@ -15,38 +17,30 @@ const About = (props) => {
             {info.label}
           </h2>
           <div className="mt-2 text-xs text-gray-500 md:hidden">
+            <div>{info.email}</div>
+            <div className="tabular-nums">{info.phone}</div>
             <div>
-              {info.email}
-            </div>
-            <div className="tabular-nums">
-              {info.phone}
-            </div>
-            <div>
-              <Link href={info.website}>
-                <a>{info.website}</a>
-              </Link>
+              <Link href={info.website}>{info.website}</Link>
             </div>
           </div>
         </div>
 
-        <div className="relative flex my-2 mx-4">
-          <Link href={twitter.url}>
-            <a className="relative z-10"><img className="w-20 h-20 md:w-16 md:h-16 rounded-full ring-4 ring-green-400 ring-offset-2 dark:ring-offset-gray-900" src={info.image} alt={`Picture of ${info.name}`} /></a>
+        {/* <div className="relative flex my-2 mx-4">
+          <Link href={twitter.url} className="relative z-10">
+            <img
+              className="w-20 h-20 md:w-16 md:h-16 rounded-full ring-4 ring-green-400 ring-offset-2 dark:ring-offset-gray-900"
+              src={info.image}
+              alt={`Picture of ${info.name}`}
+            />
           </Link>
           <div className="absolute inline-flex w-20 h-20 md:w-16 md:h-16 rounded-full ring-8 ring-green-400 animate-ping opacity-20"></div>
-        </div>
+        </div> */}
 
-        <div className="ml-2 mb-2 md:mb-0 text-xs text-gray-500 hidden md:block">
+        <div className="ml-2 mb-2 md:mb-0 text-xs text-gray-500 hidden md:block border-l pl-4 border-gray-700">
+          <div>{info.email}</div>
+          <div className="tabular-nums">{info.phone}</div>
           <div>
-            {info.email}
-          </div>
-          <div className="tabular-nums">
-            {info.phone}
-          </div>
-          <div>
-            <Link href={info.website}>
-              <a>{info.website}</a>
-            </Link>
+            <Link href={info.website}>{info.website}</Link>
           </div>
         </div>
       </div>
@@ -55,7 +49,7 @@ const About = (props) => {
         {info.summary} based in {info.location.address}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- components/Item.js
+++ components/Item.js
@@ -1,37 +1,42 @@
-import Link from 'next/link'
-import Title from './Title'
+import Link from 'next/link';
+import Title from './Title';
 
 const Item = (props) => {
   return (
-      <div className={`mb-4 pl-4 border-l-4 border-${props.endDate ? 'gray-200' : 'green-100'} dark:border-${props.endDate ? 'gray-600' : 'green-200'}`}>
-        <div className="tracking-tight flex items-baseline justify-between">
-          <div>
-            <span className="font-extrabold mr-1">{props.title}</span>
-            <small className="text-gray-500 font-medium italic">
-              {props.link && 
-                <Link href={props.link}>
-                  <a target="_blank">@ {props.location}</a>
-                </Link>
-              }
+    <div
+      className={`mb-4 pl-4 border-l-4 border-${
+        props.endDate ? 'gray-200' : 'green-100'
+      } dark:border-${props.endDate ? 'gray-600' : 'green-200'}`}
+    >
+      <div className="tracking-tight flex items-baseline justify-between">
+        <div>
+          <span className="font-extrabold mr-1">{props.title}</span>
+          <small className="text-gray-500 font-medium italic">
+            {props.link && (
+              <Link href={props.link} target="_blank">
+                @ {props.location}
+              </Link>
+            )}
 
-              {!props.link &&
-                `@ ${props.location}`
-              }
-            </small>
-          </div>
-
-          <span className="text-xs text-gray-400 font-mono md:w-auto w-12 text-center ml-2">
-            <span className="block md:inline">{props.startDate}</span>
-            <span className="px-1 hidden md:inline">-</span>
-            <span>{props.endDate ? props.endDate : 'Present'}</span>
-          </span>
+            {!props.link && `@ ${props.location}`}
+          </small>
         </div>
 
-        { props.content &&
-          <p className="text-sm mt-1 text-gray-600 dark:text-gray-500 w-3/4" dangerouslySetInnerHTML={{__html: props.content}}></p>
-        }
+        <span className="text-xs text-gray-400 font-mono md:w-auto w-12 text-center ml-2">
+          <span className="block md:inline">{props.startDate}</span>
+          <span className="px-1 hidden md:inline">-</span>
+          <span>{props.endDate ? props.endDate : 'Present'}</span>
+        </span>
       </div>
-  )
-}
 
-export default Item
+      {props.content && (
+        <p
+          className="text-sm mt-1 text-gray-600 dark:text-gray-500 w-3/4"
+          dangerouslySetInnerHTML={{ __html: props.content }}
+        ></p>
+      )}
+    </div>
+  );
+};
+
+export default Item;
